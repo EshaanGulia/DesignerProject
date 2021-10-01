fd88e10e3bdd270aef40ec60c355afd9e086bfc9
s461920 <s461920@68XTMR2>
2021-10-01 14:36:30 -0500

    Add project files.

---
+++ inChatLesson2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClasslesson1
+{
+    public partial class inChatLesson2 : Form
+    {  
+        public inChatLesson2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+            this.Controls(); 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Text = "Data";
+
+            this.Search.Text = "Search Criteria";
+        }
+         
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {ThreadExceptionDialog
+
+                private void SetControls()
+            {
+
+            }
+
+        }
+    }
+}
+public static DataSet GetExcelData();
+{
+    DataSet ds = new DataSet();
+    DataTable dt = new DataSet();
+
+    filestream stream = FileDialog.open(FileDialog,Fileemode,openFileaccess,ReadOnlyAttribute)
+}
